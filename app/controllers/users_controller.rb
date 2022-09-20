9867b52622629526921b7ecb321253231c862fcd
clpiano <111512024+clpiano@users.noreply.github.com>
2022-09-20 16:16:01 +0000

    [save]kadai3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,29 @@
 class UsersController < ApplicationController
 
   def show
-    
     @user = User.find(params[:id])
     @books = @user.books
+    @book = Book.new
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to "/users/#{@user.id}"
   end
 
   def index
-    @user = User.all
-    @books = @user.name
+    @users = User.all
+    @book = Book.new
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:name, :profile_images)
+    params.require(:user).permit(:name, :profile_image)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,8 @@ Rails.application.routes.draw do
 
   get '/homes/about' => 'homes#about', as: 'about'
 
-
-
-  resources :books, only: [:index, :show, :edit, :new, :create, :save]
-  resources :users, only: [:index, :show, :edit]
+  resources :books, only: [:index, :show, :edit, :new, :create, :update, :destroy]
+  resources :users, only: [:index, :show, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 end
