2212094265c2b198e323796ba4528f52797fd95c
clpiano <111512024+clpiano@users.noreply.github.com>
2022-09-24 06:22:52 +0000

    [save]kadai3

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   get '/home/about' => 'home#about', as: 'about'
 
-  resources :books, only: [:index, :show, :edit, :new, :create, :update, :destroy]
-  resources :users, only: [:index, :show, :edit, :update]
+  resources :books
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
